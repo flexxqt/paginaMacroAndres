498a314af6c872b2279faa99222d109e711741b6
Andres Martinez <amartinezb@correo.iue.edu.co>
2025-04-09 00:33:29 -0500

    Se agrega slide de las fotos del menu principal

---
+++ slide.js
@@ -0,0 +1,9 @@
+//slide para transicion de las fotos del menu principal
+let contador = 0;
+const slides = document.querySelectorAll('.slide');
+function fotoSiguiente() {
+    slides[contador].classList.remove('active');
+    contador = (contador + 1) % slides.length;
+    slides[contador].classList.add('active');
+}
+setInterval(fotoSiguiente, 8000); 
